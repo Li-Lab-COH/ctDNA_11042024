ff6c3fd0aa2939edb19ace670df6172f2e16e160
Jonathan Anzules <jonanzule@gmail.com>
2025-03-05 12:07:56 -0800

    fixing corrupted files

--- fragment_analysis/bedtools_bins_analysis.sh
+++ fragment_analysis/bedtools_bins_analysis.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=012:00:00             
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=32G
+#SBATCH --mem=128G
 #SBATCH --mail-user=janzules@coh.org
 #SBATCH --mail-type=FAIL
 
--- fragment_analysis/submit_array_jobs_intersect.sh
+++ fragment_analysis/submit_array_jobs_intersect.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Path to the file containing the list of folders
-FOLDER_LIST="/home/janzules/ctDNA_11042024/code/addresses/mapped.n.bin.bam.files.txt"
+# FOLDER_LIST="/home/janzules/ctDNA_11042024/code/addresses/mapped.n.bin.bam.files.txt"
+FOLDER_LIST="/home/janzules/ctDNA_11042024/code/addresses/fixingCorruption.mapped.n.bin.bam.files.txt"
 
 # Read the folder paths line by line
 while read -r FOLDER_PATH; do
